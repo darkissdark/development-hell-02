ff9116e8f210eba751d4f7bef99a8e920205de0e
Viktor Medvid <darkissdark@gmail.com>
2025-07-24 15:57:32 +0200

    Refactor footer form with validation and toast notifications; update button styles for larger screens

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,1 +1,46 @@
-console.log('Footer script loaded');
+import { showToast } from './common';
+
+const form = document.querySelector('.footer-join-form');
+const validationMessage = document.querySelector('.footer-form-validation');
+const input = document.querySelector('.footer-email');
+
+if (!form || !validationMessage || !input) {
+  console.warn('Form or input elements not found.');
+} else {
+  form.addEventListener('submit', handleSubmit);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  input.addEventListener('input', handleInputValidation);
+
+  const formData = new FormData(form);
+  const email = formData.get('footer-email').trim();
+
+  if (!email || !input.validity.valid) {
+    showError('Please enter a valid email address');
+    return;
+  }
+
+  clearError('* Required field');
+  showToast(`Thank you for subscribing with <b>${email}</b>`, 'success');
+  form.reset();
+  input.removeEventListener('input', handleInputValidation);
+}
+
+function handleInputValidation() {
+  clearError('* Required field');
+}
+
+function showError(message) {
+  validationMessage.textContent = message;
+  validationMessage.classList.add('color-error');
+  input.classList.add('error');
+}
+
+function clearError(message) {
+  validationMessage.textContent = message;
+  validationMessage.classList.remove('color-error');
+  input.classList.remove('error');
+}
