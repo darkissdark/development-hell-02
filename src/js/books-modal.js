bab2d1e99fbef3bfe1fecade4559febdf0754b7d
kar <myspecspace@gmail.com>
2025-07-25 20:12:20 +0400

    do accordion

--- src/js/books-modal.js
+++ src/js/books-modal.js
@@ -1,1 +1,11 @@
 import iziToast from 'izitoast';
+import Accordion from 'accordion-js';
+
+const refs = {
+  btnBuy: document.querySelector('.js-modal-books-buy'),
+};
+
+const accordion = new Accordion('.modal-books-accordion-container', {
+  duration: 400,
+  showMultiple: true,
+});
