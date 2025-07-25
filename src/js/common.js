3d49849f0373a4cb071efd769de5fbe2015f2711
Viktor Medvid <darkissdark@gmail.com>
2025-07-25 18:09:11 +0200

    Add shake animation for error inputs and adjust toast position based on window width

--- src/js/common.js
+++ src/js/common.js
@@ -5,7 +5,7 @@ const windowWidth = window.innerWidth;
 export function showToast(message, type = 'error') {
   iziToast[type]({
     message,
-    position: 'topRight',
+    position: windowWidth < 768 ? 'center' : 'topRight',
     maxWidth: windowWidth < 432 ? 300 : 400,
   });
 }
