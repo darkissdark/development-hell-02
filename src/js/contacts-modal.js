ca11332e79312aa8879375223213175457d3b676
Fed Kon <fedkon96@gmail.com>
2025-07-28 18:46:16 +0300

    start validation

--- src/js/contacts-modal.js
+++ src/js/contacts-modal.js
@@ -1,15 +1,48 @@
 import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const refs = {
-  openFormModal: document.querySelector('.register-btn'),
-  formBackdrop: document.querySelector('.modal-backdrop-form'),
+  openFormModal: document.querySelectorAll('.register-btn'),
+  formBackdrop: document.querySelector('#modalContacts'),
   closeBtnModal: document.querySelector('.modal-form-close'),
   formList: document.querySelector('.form-box'),
   inputName: document.querySelector('[data-name]'),
   inputEmail: document.querySelector('[data-email]'),
   inputArea: document.querySelector('[data-area]'),
   submitBtnModal: document.querySelector('.form-btn-submit'),
+  titleEventSection: document.querySelectorAll('.event-subhead'),
+  titleEventModal: document.querySelector('.modal-form-subscription'),
 };
+
+// ! TITLE
+const originalTitles = [];
+
+function transferEventTitle(btn) {
+  const eventCard = btn.closest('.event-card');
+  const titleElement = eventCard
+    ? eventCard.querySelector('.event-subhead')
+    : null;
+
+  if (titleElement) {
+    const titleText = titleElement.textContent;
+
+    if (!originalTitles.includes(titleText)) {
+      originalTitles.push(titleText);
+    }
+
+    refs.titleEventModal.textContent = titleText;
+  }
+}
+
+function resetEventTitle() {
+  document.querySelectorAll('.event-subhead').forEach((titleElement, index) => {
+    if (originalTitles[index]) {
+      titleElement.textContent = originalTitles[index];
+    }
+  });
+
+  refs.titleEventModal.textContent = 'Cozy Book Club — "The Midnight Library"';
+}
 
 // ! OPEN / CLOSE MODAL
 const closeBtn = () => {
@@ -32,27 +65,39 @@ function addEventListeners() {
   refs.closeBtnModal.addEventListener('click', closeModal);
   refs.formBackdrop.addEventListener('click', closeBackdrop);
   document.addEventListener('keydown', closeEscape);
+
+  refs.formList.addEventListener('input', handleInput);
+  refs.formList.addEventListener('submit', handleSubmit);
 }
 
 function removeEventListeners() {
   refs.closeBtnModal.removeEventListener('click', closeModal);
   refs.formBackdrop.removeEventListener('click', closeBackdrop);
   document.removeEventListener('keydown', closeEscape);
+
+  refs.formList.removeEventListener('input', handleInput);
+  refs.formList.removeEventListener('submit', handleSubmit);
 }
 
-function openModal() {
+function openModal(e) {
+  const btn = e.currentTarget;
+  transferEventTitle(btn);
   refs.formBackdrop.classList.add('is-open');
   document.body.classList.add('no-scroll');
   addEventListeners();
 }
 
-function closeModal() {
+function closeModal(e) {
   refs.formBackdrop.classList.remove('is-open');
   document.body.classList.remove('no-scroll');
   removeEventListeners();
+  resetEventTitle();
 }
 
-// refs.openFormModal.addEventListener('click', openModal);
+// !!! LISRENER
+refs.openFormModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
 
 refs.closeBtnModal.addEventListener('click', closeBtn);
 
@@ -60,7 +105,10 @@ document.addEventListener('keydown', closeEscape);
 
 document.addEventListener('click', closeBackdrop);
 
+// ! VALIDATION
+
 // ! LOCAL STORAGE
+
 function saveToLocalStorage() {
   const formData = {
     name: refs.inputName.value.trim(),
